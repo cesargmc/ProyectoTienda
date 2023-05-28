f633bda6644e3fd0e274b550ab02f1975144f6cf
unknown <victortrevinovilla@gmail.com>
2023-05-28 15:30:32 -0600

    Agregue alerta de error en iniciosesion

--- iniciosesion.php
+++ iniciosesion.php
@@ -7,8 +7,6 @@
     $errores = [];
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        var_dump($_POST);
-
         $email = mysqli_real_escape_string($db, filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL ));
         $password = $_POST['password'];
     
@@ -21,13 +19,18 @@
         }
     }
 
-
     $login = true;
     include './includes/templates/header.php';
 ?>
 
     <main class="contenedor">
         <h1>Iniciar Sesion</h1>
+
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach ?>
 
         <div class="cuenta">
             <form class="formulario" method="POST">
