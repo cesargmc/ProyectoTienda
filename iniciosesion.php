3ea9302461f730b5cb774321dd26c12403fc580d
unknown <victortrevinovilla@gmail.com>
2023-05-28 15:00:05 -0600

    modificacion en div class cuenta en iniciosesion

--- iniciosesion.php
+++ iniciosesion.php
@@ -26,9 +26,9 @@
         <h1>Iniciar Sesion</h1>
 
         <div class="cuenta">
-            <form class="formulario">
-                <input class="formulario__campo" type="text" placeholder="Ingrese su usuario">
-                <input class="formulario__campo" type="password" placeholder="Ingrese su contraseña">
+            <form class="formulario" method="POST">
+                <input class="formulario__campo" name = "email" type="text" placeholder="Ingrese su usuario">
+                <input class="formulario__campo" name = "password" type="password" placeholder="Ingrese su contraseña">
                 <input class="formulario__submit" type="submit" value="Ingresar">
                 <a href="crearcuenta.php"><p>¿No tienes una cuenta?</p></a>
             </form>
