565727818ac423573561e3750feb00415bc01a20
= <=>
2023-05-27 22:12:16 -0600

    1

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -81,7 +81,7 @@
             <a class="navegacion-admin__enlace" href="mensajes.php">Mensajes</a>
             <a class="navegacion-admin__enlace" href="iniciosesion.php">Cerrar Sesion</a>
         </nav>
-    <?php } else { if  ($mensaje) { ?>
+    <?php } elseif  ($mensaje) { ?>
         <nav class="navegacion-admin">
             <a class="navegacion-admin__enlace" href="index.php">Inicio</a>
             <a class="navegacion-admin__enlace" href="productos.php">Productos</a>
@@ -90,8 +90,4 @@
             <a class="navegacion-admin__enlace navegacion-admin__enlace--activo" href="mensajes.php">Mensajes</a>
             <a class="navegacion-admin__enlace" href="iniciosesion.php">Cerrar Sesion</a>
         </nav>
-<<<<<<< HEAD
     <?php }?>
-=======
-    <?php }} ?>
->>>>>>> a1424a72be660b908b33d07876867e0dc7da42f7
