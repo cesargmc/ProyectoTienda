f6970ea43c78d81a8537f44cb5f3d1a27e39c317
unknown <victortrevinovilla@gmail.com>
2023-05-28 18:33:51 -0600

    Añadi verficiacion de usuario y su rol en pagina productos

--- admin/productos.php
+++ admin/productos.php
@@ -2,6 +2,22 @@
     // Base de datos
     require '../includes/config/database.php';
     $db = conectarDB();
+    
+    session_start();
+
+    // Verificar si el usuario ha iniciado sesión
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: iniciosesion.php");
+        exit();
+    }
+
+    // Verificar el rol del usuario
+    $rol_id = $_SESSION['usuario']['rol_id'];
+    if ($rol_id != 1) {
+        // El usuario no es administrador
+        header("Location: ../index.php");
+        exit();
+    }
 
     // Escribir el Query
     $query = "SELECT * FROM producto";
