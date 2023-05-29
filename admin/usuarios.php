ac7b8c2b1da6a44027601ae2e0ce4518cf135e1e
unknown <victortrevinovilla@gmail.com>
2023-05-28 18:38:05 -0600

    Añadi la verificacion de usuario y su rol en la pagina de usuarios de administrador

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -2,7 +2,23 @@
     //Base de datos 
     require 'includes/config/database.php';
     $db = conectarDB();
+    
+    session_start();
 
+    // Verificar si el usuario ha iniciado sesión
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: iniciosesion.php");
+        exit();
+    }
+
+    // Verificar el rol del usuario
+    $rol_id = $_SESSION['usuario']['rol_id'];
+    if ($rol_id != 1) {
+        // El usuario no es administrador
+        header("Location: ../index.php");
+        exit();
+    }
+    
     //Consultar usuarios
     $consulta = "SELECT * FROM usuario";
     $resultado = mysqli_query($db, $consulta);
