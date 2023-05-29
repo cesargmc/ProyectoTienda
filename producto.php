703af63278c54412f9f3605b6bcd0d6069023748
= <=>
2023-05-29 01:58:59 -0600

    modificacion del form para compra

--- producto.php
+++ producto.php
@@ -22,8 +22,11 @@
 
     $producto = mysqli_fetch_assoc($resultado);
 
+    // Guardar información de la imagen en una variable de sesión
+    $imagenProducto = $producto['imagen'];
+
     $consulta = "SELECT * FROM talla";
-    $resultado = mysqli_query($db, $consulta);
+    $resultadoTallas = mysqli_query($db, $consulta);
     
     $productoI = true;
     include './includes/templates/header.php';
@@ -80,6 +83,8 @@
         
                 echo "<script>alert('Compra realizada correctamente');</script>";
             }
+        } else {
+            echo "<script>alert('Compra realizada incorrectamente');</script>";
         }
     }
 ?>
@@ -88,19 +93,20 @@
         <h1><?php echo $producto['nombre']; ?></h1>
 
         <div class="camisa">
-            <img class="camisa__imagen" src="/imagenes/<?php echo $producto['imagen']; ?>" alt="Imagen del producto">
+            <img class="camisa__imagen" src="/imagenes/<?php echo $imagenProducto; ?>" alt="Imagen del producto">
 
             <div class="camisa__contenido">
                 <p><?php echo $producto['descripcion']; ?></p>
 
-                <form class="formulario">
-                    <select class="formulario__campo">
+                <form class="formulario" method="POST">
+                    <input type="hidden" name="id_producto" value="<?php echo $producto['id_producto']; ?>">
+                    <select class="formulario__campo" name="talla">
                         <option disabled selected value="">-- Seleccionar Talla --</option>
-                        <?php while($row = mysqli_fetch_assoc($resultado) ) : ?>
+                        <?php while($row = mysqli_fetch_assoc($resultadoTallas) ) : ?>
                             <option value="<?php echo $row['id_talla']; ?>"><?php echo $row['descripcion'] . ""; ?></option>
                         <?php endwhile ?>
                     </select>
-                    <input class="formulario__campo" type="number" placeholder="Cantidad" min="1">
+                    <input class="formulario__campo" type="number" name="cantidad" placeholder="Cantidad" min="1">
                     <input class="formulario__submit" type="submit" value="Comprar">
                 </form>
             </div>
