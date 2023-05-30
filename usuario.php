f82de6172d47104d6987ce6ad94cfff22f0995f5
cesargmc <cesar.garciamcs@outlook.com>
2023-05-30 13:52:45 -0600

    verificacion inicio de sesion y admin

--- usuario.php
+++ usuario.php
@@ -1,8 +1,23 @@
 <?php
-
     // Base de datos
     require 'includes/config/database.php';
     $db = conectarDB();
+
+    session_start();
+
+    // Verificar si el usuario ha iniciado sesión
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: iniciosesion.php");
+        exit();
+    }
+
+    // Verificar el rol del usuario
+    $rol_id = $_SESSION['usuario']['rol_id'];
+    if ($rol_id != 1) {
+        // El usuario no es administrador
+        header("Location: ../index.php");
+        exit();
+    }
 
     // Crear email y password
     $email = 'admin@admin.com';
